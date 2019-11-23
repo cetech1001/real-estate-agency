e35a0f9fd846f86b10a14ebcb72946f36f6e7d53
chiemerie.ezeoke <chiemerie.ezeoke@fastestbooking.com>
2019-11-23 17:13:01 +0200

    created agents pages and routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from 'express';
 
 import listingsRouter from './listings';
+import adminRouter from './admin';
+import agentRouter from './agent';
 
 
 const router = Router();
@@ -21,6 +23,16 @@ router.get('/faq', (req: Request, res: Response) => {
   res.render('faq', { title: 'Frequently Asked Questions' });
 });
 
+router.get('/agents', (req: Request, res: Response) => {
+  res.render('agents', { title: 'Agents' });
+});
+
+router.get('/agents/:id', (req: Request, res: Response) => {
+  res.render('agents/single', { title: 'Single Agent' });
+});
+
 router.use('/listings', listingsRouter);
+router.use('/admin', adminRouter);
+router.use('/agent', agentRouter);
 
 export default router;
