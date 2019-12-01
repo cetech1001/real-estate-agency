16f77bc7cf89b07292e465c8d1ed2bc38ff61ae2
chiemerie.ezeoke <chiemerie.ezeoke@fastestbooking.com>
2019-12-01 05:29:25 +0200

    fixed pagination issues

--- src/routes/agents/index.ts
+++ src/routes/agents/index.ts
@@ -2,30 +2,49 @@ import { Router } from 'express';
 import { getSessionAlerts, resetSessionAlerts } from '../../utils/alerts';
 import User from '../../models/user';
 import { serverErrorHandler } from '../../utils/error-handlers';
+import { UserInterface } from '../../interfaces/user';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
   try {
-    const agentsPerPage = 3;
+    const agentsPerPage = 4;
     const agents = await User.find({ role: 'agent' });
+    const totalAgents = agents.length;
     const pages = Math.ceil(agents.length / 3);
-    const page = req.query.page || 1;
-    const pageAgents = [];
+
+    const page: {
+      number: number;
+      agents: UserInterface[];
+      startIndex: number;
+      endIndex: number;
+    } = {
+      number: parseInt(req.query.page, 10) || 1,
+      agents: [],
+      startIndex: 0,
+      endIndex: 0,
+    };
+
     const alerts = getSessionAlerts(req);
+    resetSessionAlerts(req);
+
+    page.startIndex = (page.number - 1) * agentsPerPage;
+    page.endIndex = page.startIndex;
 
     // eslint-disable-next-line max-len
-    for (let i = (page - 1) * agentsPerPage, count = 0; count < agentsPerPage && agents[i]; count += 1, i += 1) {
-      pageAgents.push(agents[i]);
+    for (let i = page.startIndex, count = 0; count < agentsPerPage && agents[i]; i += 1, count += 1) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      page.agents.push(agents[i]);
+      page.endIndex += 1;
     }
-
-    resetSessionAlerts(req);
     res.render('agents', {
       title: 'Agents',
       ...alerts,
-      agents: pageAgents,
       pages,
       page,
+      agents: page.agents,
+      totalAgents,
     });
   } catch (e) {
     serverErrorHandler(e, res);
